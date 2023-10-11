d0f1e50d2c1e38b437e0d64ff0c9376689ffcc3b
ваше_имя <oklikoff@gmail.com>
2023-10-11 19:14:20 +0300

    addition films

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Group
 # Create your views here.
 def index(req):
     numkino=Kino.objects.all().count()
@@ -57,11 +57,15 @@ def status(req):
     k1=Status.objects.all()
     data={'podpiska':k1}
     return render(req, 'podpiska.html',context=data)
+
+
+
 def prosmotr(req,id1,id2,id3):
     print(id1,id2,id3)
-    mas=['бесплатная','базовая','супер']#kino id2
-    mas2=['free','based','super']#user id3
-    status=0
+    mas=['бесплатно','базовая','супер']#kino id2
+    mas2=['free','based','super']#user id3 status
+    status = 0
+
     if id3!=0:
         status = User.objects.get(id=id3)#нашли юзера
         status=status.groups.all()#нашли его подписку
@@ -72,6 +76,25 @@ def prosmotr(req,id1,id2,id3):
             status=1
     if status>=id2:#сравниваем статус и разрешение фильма
         print('ok')
+        permission=True
     else:
         print('nelza')
-    return render(req,'index.html')
+        permission = False
+
+    k1=Kino.objects.get(id=id1).title
+    k2=Group.objects.get(id=status).name
+    k3=Status.objects.get(id=id2).name
+    data={'kino':k1,'status':k2,'statuskino':k3,'prava':permission}
+    return render(req,'prosmotr.html',data)
+
+def buy(req,type):
+    usid=req.user.id #находим номер текущего пользователя
+    user123=User.objects.get(id=usid) #находим в табличке user
+    statusnow=user123.groups.all()[0].id #номер его подписки (группы)
+    grold = Group.objects.get(id=statusnow) #находим его подписку в таблице group
+    grold.user_set.remove(user123) #удаляем старую подписку
+    grnew=Group.objects.get(id=type) #находим новую подписку в таблице group
+    grnew.user_set.add(user123) #добавляем новую подписку
+    k1=grnew.name
+    data={'podpiska':k1}
+    return render(req,'buy.html',data)
